b68dad84c2a883be025939361b0097dd988a55b4
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2026-03-14 14:41:44 +0300

    Разделяем ресурсы на чанки

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,25 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/admin/'
+  plugins: [
+    react(),
+  ],
+  base: '/admin/',
+  build: {
+    sourcemap: false, // Отключите source maps для продакшена
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Выносим MUI Material в отдельный файл (тяжёлый)
+          'mui-material': ['@mui/material', '@emotion/react', '@emotion/styled'],
+          // Выносим MUI Joy отдельно (если используется)
+          'mui-joy': ['@mui/joy'],
+          // Выносим графики отдельно
+          'mui-charts': ['@mui/x-charts'],
+          // React и роутер
+          'react-vendor': ['react', 'react-dom', 'react-router'],
+        },
+      },
+    },
+  }
 })
